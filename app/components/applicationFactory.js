c08e1af91afd3344eaee22e34a63d74030207ed2
Austin SA Wang <austin.sa.wang@gmail.com>
2015-02-01 11:45:10 -0500

    Revert "Refactor: Extract data persistance logics into its own factory" Forgot to include updated index.html
    This reverts commit 4719de2072501ecdb0c4fad7cfb95878a38e024a.

--- app/components/applicationFactory.js
+++ app/components/applicationFactory.js
@@ -4,12 +4,30 @@
  * ApplicationFactory
  * Manage applications.
  */
-angular.module('applicationFactory', ['dataPersistenceFactory'])
+angular.module('applicationFactory', ['applicationStatusFactory'])
 
-  .factory('ApplicationFactory', function ApplicationFactory (DataPersistenceFactory) {
+  .factory('ApplicationFactory', function ApplicationFactory (ApplicationStatusFactory) {
     var STORAGE_IDENTIFIER = 'applications';
 
-    var applications = DataPersistenceFactory.getApplications();
+    var applications = (/**
+       * Initialize applications list from localStorage, replacing status names with status objects
+       * Initialize to empty array if existing list does not exist
+       * @returns {Array}
+       */
+        function () {
+        var applications = JSON.parse(localStorage.getItem(STORAGE_IDENTIFIER));
+        if (Object.prototype.toString.call(applications) === '[object Array]') {
+          // Replace status names with status objects
+          var currentAppl;
+          for (var i = 0; i < applications.length; i+=1) {
+            currentAppl = applications[i];
+            currentAppl.status = ApplicationStatusFactory.getStatusObject(currentAppl.status);
+          }
+          return applications;
+        } else {
+          return [];
+        }
+      })();
 
     // TOFIX: This doesn't protect the data
     ApplicationFactory.getApplications = function () {
@@ -24,7 +42,7 @@ angular.module('applicationFactory', ['dataPersistenceFactory'])
       applications.push(applicationEntry);
 
       // TODO: Reverse the direction. Observe changes to applications instead of calling manually
-      ApplicationFactory.persist();
+      ApplicationFactory.updateStorage();
     };
 
     /**
@@ -35,7 +53,7 @@ angular.module('applicationFactory', ['dataPersistenceFactory'])
       applications.splice(applications.indexOf(applicationEntry), 1);
 
       // TODO: Reverse the direction. Observe changes to applications instead of calling manually
-      ApplicationFactory.persist();
+      ApplicationFactory.updateStorage();
     };
 
     /**
@@ -47,7 +65,7 @@ angular.module('applicationFactory', ['dataPersistenceFactory'])
       applicationEntry.status = status;
 
       // TODO: Reverse the direction. Observe changes to applications instead of calling manually
-      ApplicationFactory.persist();
+      ApplicationFactory.updateStorage();
     };
 
     /**
@@ -68,7 +86,7 @@ angular.module('applicationFactory', ['dataPersistenceFactory'])
       applicationEntry.note = newNote;
 
       // TODO: Reverse the direction. Observe changes to applications instead of calling manually
-      ApplicationFactory.persist();
+      ApplicationFactory.updateStorage();
     };
 
     /**
@@ -81,11 +99,30 @@ angular.module('applicationFactory', ['dataPersistenceFactory'])
     };
 
     /**
+     * JSON replacer function to
+     * 1. remove AngularJS ngRepeat array bookkeeping overhead, and
+     * 2. replace status object with its name.
+     *
+     * @param key
+     * @param value
+     * @returns {*}
+     */
+    var toJsonReplacer = function (key, value) {
+      var val = value;
+      if (typeof key === 'string' && key.charAt(0) === '$' && key.charAt(1) === '$') {
+        val = undefined;
+      } else if (key === 'status') {
+        val = value.name;
+      }
+      return val;
+    };
+
+    /**
      * Update local storage to persist state
      * TODO: Currently this is triggered manually. Ideally we should observe the model and update accordingly.
      */
-    ApplicationFactory.persist = function () {
-      DataPersistenceFactory.persist(applications);
+    ApplicationFactory.updateStorage = function () {
+      localStorage.setItem(STORAGE_IDENTIFIER, JSON.stringify(applications, toJsonReplacer));
     };
 
     return ApplicationFactory;
